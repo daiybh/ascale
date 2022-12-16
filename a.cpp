5ef8e454997bfafa9b1351b1e3404a29d0cf08da
dai Li <daiybh@gmail.com>
2022-12-16 13:21:09 +0800

    work HD->480,270

--- a.cpp
+++ a.cpp
@@ -10,8 +10,7 @@ void covert()
     uint8_t *sourceBuffer = new uint8_t[1920 * 1080 * 10];
     FILE *fps, *fpD;
     fopen_s(&fps, R"(D:\clips\Cam(A).live.yuv)", "rb");
-    fopen_s(&fpD, "d:\\clips\\aa_1920x1080_uyvy.yuv", "wb");
-
+    
     fseek(fps, 0, SEEK_END);
     int nLength = ftell(fps);
     rewind(fps);
@@ -21,8 +20,16 @@ void covert()
 
     // The data is arranged in blocks of 4 samples. How many of these are there?
    // Scaler_1920_1080::V210_to_960_540_YUV(targetBuffer,sourceBuffer);
-Scaler_1920_1080YUV422::V210_to_960_540_YUV420(targetBuffer,sourceBuffer);
+    Scaler_1920_1080YUV422::V210_to_960_540_YUV420(targetBuffer,sourceBuffer);
+    fopen_s(&fpD, "d:\\clips\\aa_960x540_uyvy.yuv", "wb");
     fwrite(targetBuffer, 1920 * 1080 * 2, 1, fpD);
+    fclose(fpD);
+
+    Scaler_1920_1080YUV422::V210_to_480_270_YUV420(targetBuffer,sourceBuffer);
+    fopen_s(&fpD, "d:\\clips\\aa_480x270_uyvy.yuv", "wb");
+    fwrite(targetBuffer, 1920 * 1080 * 2, 1, fpD);
+    fclose(fpD);
+
 }
 
 int main()
--- scaler1080_yuv422.h
+++ scaler1080_yuv422.h
@@ -45,50 +45,161 @@ namespace Scaler_1920_1080YUV422
 
 	void V210_to_960_540_YUV420(unsigned char* small960Frame, unsigned char* fullFrame)
 	{
-		uint8_t* pDestY = small960Frame;            // dest
-		uint8_t* pDestU = pDestY + 1920 * 1080;     // dest
-		uint8_t* pDestV = pDestU + 1920 * 1080 / 4; // dest
-		unsigned char* cur;
-		int t_width = 0;
-		int destWidth = 960;
-		t_width = destWidth * 3;
+		int destW=960;
+		int destH=540;
+		int scaleX=1920/destW;
+		int scaleY=1080/destH;
 
-#define clip(var) ((var >= 255) ? 255 : (var <= 0) ? 0 \
-                                                   : var)
+		uint8_t* pDestY = small960Frame;            // dest
+		uint8_t* pDestU = pDestY + destW*destH;     // dest
+		uint8_t* pDestV = pDestU + ( destW*destH) / 4; // dest
+
 #define GET_linePitch(w) (((w + 47) / 48) * 48)
 
 		int linePitch = GET_linePitch(1920);
 		int lineSize = (1920 / 48 + ((1920 % 48) ? 1 : 0)) * 128;
 		unsigned int* buffer = nullptr;
 		int32_t step = linePitch / 6;
-		for (int h = 0; h < 1080; h++)
+
+		for (int h = 0,iline=0; h < 1080; h+=scaleY)
 		{
 			buffer = (unsigned int*)(fullFrame + h * lineSize);
-			uint32_t* nextLineBuffer = (uint32_t*)(fullFrame + (h+1) * lineSize);
+			uint32_t* nextLineBuffer = (uint32_t*)(fullFrame + (h +scaleY) * lineSize);
+
+			int iU=0;
+			int iY=0;
 			for (int i = 0; i < step; i++)
 			{
 				Pixel2_1xV210* p = (Pixel2_1xV210*)buffer;
-				*pDestY++ = p->Y0;
-				*pDestY++ = p->Y1;
-				*pDestY++ = p->Y2;
-				*pDestY++ = p->Y3;
-				*pDestY++ = p->Y4;
-				*pDestY++ = p->Y5;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y0;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y1;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y2;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y3;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y4;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y5;
 				
-				if (h % 2 == 0)
+				if (iline % scaleY == 0)
 				{
-					Pixel2_1xV210* np = (Pixel2_1xV210*)nextLineBuffer;
+					Pixel2_1xV210 *np = (Pixel2_1xV210 *)nextLineBuffer;
 
-					*pDestU++ = (p->U0+  np->U0)/2;
-					*pDestU++ = (p->U1 + np->U1)/2;
-					*pDestU++ = (p->U2 + np->U2)/2;
-					*pDestV++ = (p->V0 + np->V0)/2;
-					*pDestV++ = (p->V1 + np->V1)/2;
-					*pDestV++ = (p->V2 + np->V2)/2;
+					if (iU++ % scaleX == 0)
+					{
+						*pDestU++ = (p->U0 + np->U0) / 2;
+						*pDestV++ = (p->V0 + np->V0) / 2;
+					}
+					if (iU++ % scaleX == 0)
+					{
+						*pDestU++ = (p->U1 + np->U1) / 2;
+						*pDestV++ = (p->V1 + np->V1) / 2;
+					}
+					if (iU++ % scaleX == 0)
+					{
+						*pDestU++ = (p->U2 + np->U2) / 2;
+						*pDestV++ = (p->V2 + np->V2) / 2;
+					}
 				}
+
 				buffer += 4;
 				nextLineBuffer += 4;
 			}
+			iline++;
+		}
+	}
+	/*
+	1920/480= 4
+	1080/270 = 4
+
+	1280/480= 2.6666
+		3-->1
+		3-->1
+		2-->1 
+
+		8  3
+		1280/480 128/48 16/6  8/3 
+
+	720/270 = 2.6666
+	
+	72/27  8/3
+	     3-->1
+		 3-->1
+		 2-->1
+		 
+	
+	*/
+
+	void V210_to_480_270_YUV420(unsigned char* small960Frame, unsigned char* fullFrame)
+	{		
+		int destW=480;
+		int destH=270;
+		
+		int scaleX=1920/destW;
+		int scaleY=1080/destH;
+
+		uint8_t* pDestY = small960Frame;            // dest
+		uint8_t* pDestU = pDestY + destW*destH;     // dest
+		uint8_t* pDestV = pDestU + ( destW*destH) / 4; // dest
+
+#define GET_linePitch(w) (((w + 47) / 48) * 48)
+
+		int linePitch = GET_linePitch(1920);
+		int lineSize = (1920 / 48 + ((1920 % 48) ? 1 : 0)) * 128;
+		unsigned int* buffer = nullptr;
+		int32_t step = linePitch / 6;
+
+		for (int h = 0,iline=0; h < 1080; h+=scaleY)
+		{
+			buffer = (unsigned int*)(fullFrame + h * lineSize);
+			uint32_t* nextLineBuffer = (uint32_t*)(fullFrame + (h +scaleY) * lineSize);
+
+			int iU=0;
+			int iY=0;
+			for (int i = 0; i < step; i++)
+			{
+				Pixel2_1xV210* p = (Pixel2_1xV210*)buffer;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y0;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y1;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y2;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y3;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y4;
+				if (iY++ % scaleX == 0)
+					*pDestY++ = p->Y5;
+				
+				if (iline % 2 == 0)
+				{
+					Pixel2_1xV210 *np = (Pixel2_1xV210 *)nextLineBuffer;
+
+					if (iU++ % scaleX == 0)
+					{
+						*pDestU++ = (p->U0 + np->U0) / 2;
+						*pDestV++ = (p->V0 + np->V0) / 2;
+					}
+					if (iU++ % scaleX == 0)
+					{
+						*pDestU++ = (p->U1 + np->U1) / 2;
+						*pDestV++ = (p->V1 + np->V1) / 2;
+					}
+					if (iU++ % scaleX == 0)
+					{
+						*pDestU++ = (p->U2 + np->U2) / 2;
+						*pDestV++ = (p->V2 + np->V2) / 2;
+					}
+				}
+
+				buffer += 4;
+				nextLineBuffer += 4;
+			}
+			iline++;
 		}
 	}
 };
