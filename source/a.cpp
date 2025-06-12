008c36c8976bb2c06e00a34e6b1df72bbc469771
Dai li <li.dai@riedel.net>
2025-06-13 01:22:28 +0200

    feat(v210): add new scaling functions for V210 to YUV conversions

--- source/a.cpp
+++ source/a.cpp
@@ -11,38 +11,114 @@
 #include "P216_toAny.h"
 #include "PA16_toAny.h"
 #include "define.h"
-
+#include <functional>
 #include <chrono>
 using namespace std::chrono;
 void test_v210_toAny_FPS()
 {
-    int w = 3840,h = 2160;
+	int w = 3840, h = 2160;
 	uint8_t* targetUYVY8_Buffer = new uint8_t[w * h * 10];
 	uint8_t* targetYUV10Packing_Buffer = new uint8_t[w * h * 10];
 	uint8_t* sourceBuffer = new uint8_t[w * h * 10];
-	
+
 	memset(targetUYVY8_Buffer, 0, w * h * 10);
 
 
+	auto pElapsedFunc = [&](std::function<void()> func) {
+		time_point<std::chrono::system_clock> start = system_clock::now();
+		int runCount = 10000;
+		for (int i = 0; i < runCount; i++)
+			func();
+
+		time_point<system_clock> end = system_clock::now();
+
+		std::chrono::duration<double> elapsed = end - start;
+		float el = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
+		std::cout << "\tElapsed time: "
+			<< el << "ms/" << runCount << " "
+			<< el / runCount << "ms/perframe" << std::endl;
+		//  return std::chrono::duration_cast<std::chrono::seconds>(elapsed).count();
+		};
 	auto PFunc = [&](int sourceW, int sourceH, int destW, int destH)
 		{
+			std::cout << std::format("{}x{}-->{}x{}", sourceW, sourceH, destW, destH);
+			
+			pElapsedFunc([&]() {
+				v210::V210_to_YUV_Any(targetUYVY8_Buffer, sourceBuffer, sourceW, sourceH, destW, destH);
+				});
 
-			time_point<std::chrono::system_clock> start = system_clock::now();
 
-			for(int i=0;i<1000;i++)
-				v210::V210_to_YUV_Any(targetUYVY8_Buffer, sourceBuffer, sourceW, sourceH,destW,destH);
-
-			time_point<system_clock> end = system_clock::now();
-
-			std::chrono::duration<double> elapsed = end - start;
-			std::cout<<std::format("{}x{}-->{}x{}",sourceW,sourceH,destW,destH) << "Elapsed time: " << elapsed.count() << "s" << "/" << 1000 << " pertimes:" << elapsed.count() / 1000 << std::endl;
+			
 
 		};
-    PFunc(1920, 1080, 960, 540);
-    PFunc(1920, 1080, 480, 270);
+	/*PFunc(1920, 1080, 960, 540);
+	PFunc(1920, 1080, 480, 270);
 
 	PFunc(3840, 2160, 960, 540);
-	PFunc(3840, 2160, 480, 270);
+	PFunc(3840, 2160, 480, 270);*/
+	{
+		std::cout << "1920x1080-->480x270 V210_to_YUV_4_to_1";
+		pElapsedFunc([&]() {
+			v210::V210_to_YUV_4_to_1(targetUYVY8_Buffer, sourceBuffer, 1920, 1080,480,270);
+			});
+	}
+	{
+		std::cout << "1920x1080-->480x270       OLD         ";
+		pElapsedFunc([&]() {
+
+			v210::Scaler_1920_1080_V210_to_YUV480x270(targetUYVY8_Buffer, sourceBuffer);
+			});
+
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "1920x1080-->960x540 V210_to_YUV_2_to_1";
+		pElapsedFunc([&]() {
+			v210::V210_to_YUV_2_to_1(targetUYVY8_Buffer, sourceBuffer, 1920, 1080,960,540);
+			});
+	}
+
+	{
+		std::cout << "1920x1080-->960x540       OLD         ";
+		pElapsedFunc([&]() {
+			Scaler_1920_1080::V210_to_960_540_YUV(targetUYVY8_Buffer, sourceBuffer);
+			});
+
+	}
+
+	std::cout << std::endl;
+	{
+		std::cout << "3840x2160-->480x270 V210_to_YUV_8_to_1";
+		pElapsedFunc([&]() {
+			v210::V210_to_YUV_4_to_1(targetUYVY8_Buffer, sourceBuffer, 3840,2160, 480, 270);
+			});
+	}
+
+	{
+		std::cout << "3840x2160-->480x270       OLD         ";
+		pElapsedFunc([&]() {
+			v210::Scaler_3840_2160_V210_to_YUV480x270(targetUYVY8_Buffer, sourceBuffer);
+			});
+
+	}
+
+	std::cout << std::endl;
+	{
+		std::cout << "3840x2160-->960x540 V210_to_YUV_4_to_1";
+		pElapsedFunc([&]() {
+			v210::V210_to_YUV_4_to_1(targetUYVY8_Buffer, sourceBuffer, 3840, 2160, 960,540);
+			});
+	}
+	{
+		std::cout << "3840x2160-->960x540       OLD         ";
+		pElapsedFunc([&]() {
+			v210::Scaler_3840_2160_V210_to_YUV960x540(targetUYVY8_Buffer, sourceBuffer);
+			});
+
+	}
+
+	getchar();
+	
 }
 void test_1080V210(const char* _destFolder)
 {
@@ -74,24 +150,31 @@ void test_1080V210(const char* _destFolder)
 		fclose(fpD);
 	}
 
-	
 
-    w = 1920; h = 1080;
+
+	w = 1920; h = 1080;
 	memset(targetUYVY8_Buffer, 0, w * h * 10);
-	v210::V210_to_YUV_Any(targetUYVY8_Buffer, sourceBuffer,1920,1080,960,540);
+
+	v210::V210_to_YUV_4_to_1(targetUYVY8_Buffer, sourceBuffer, 1920, 1080, 480,270);
 	{
-		fpD = fopen(getDestFilePath("V210_to_YUV_Any_960x540_2"), "wb");
-		fwrite(targetUYVY8_Buffer, 960*540 * 3, 1, fpD);
+		fpD = fopen(getDestFilePath("V210_to_YUV_4_to_1_480x270"), "wb");
+		fwrite(targetUYVY8_Buffer, 480 * 270 * 3, 1, fpD);
 		fclose(fpD);
 	}
-    memset(targetUYVY8_Buffer, 0, w * h * 10);
-	v210::V210_to_YUV_Any(targetUYVY8_Buffer, sourceBuffer, 1920, 1080, 480,270);
+	v210::V210_to_YUV_Any(targetUYVY8_Buffer, sourceBuffer, 1920, 1080, 960, 540);
+	{
+		fpD = fopen(getDestFilePath("V210_to_YUV_Any_960x540_2"), "wb");
+		fwrite(targetUYVY8_Buffer, 960 * 540 * 3, 1, fpD);
+		fclose(fpD);
+	}
+	memset(targetUYVY8_Buffer, 0, w * h * 10);
+	v210::V210_to_YUV_Any(targetUYVY8_Buffer, sourceBuffer, 1920, 1080, 480, 270);
 	{
 		fpD = fopen(getDestFilePath("V210_to_YUV_Any_480x270"), "wb");
 		fwrite(targetUYVY8_Buffer, 480 * 270 * 3, 1, fpD);
 		fclose(fpD);
 	}
-    w = 3840; h = 2160;
+	w = 3840; h = 2160;
 	v210::V210_to_YUV_Any(targetUYVY8_Buffer, sourceBuffer, 3840, 2160, 480, 270);
 	{
 		fpD = fopen(getDestFilePath("V210_to_YUV_Any_480x270"), "wb");
@@ -99,115 +182,114 @@ void test_1080V210(const char* _destFolder)
 		fclose(fpD);
 	}
 }
-void test_V210_toAny(const char *_destFolder,int w,int h)
-{ 
-    char destFolder[255];
-    strcpy(destFolder,_destFolder);
-    strcat(destFolder,"\\v210\\");
-    std::filesystem::create_directories(destFolder);
-
-    uint8_t * targetUYVY8_Buffer = new uint8_t[w*h * 10];
-    uint8_t * targetYUV10Packing_Buffer = new uint8_t[w*h* 10];
-    uint8_t* sourceBuffer = new uint8_t[w*h* 10];
-    FILE *fps, *fpD;
-    if(h==1080)
-        fps= fopen( R"(d:\clips\v210_1080.yuv)", "rb");
-    else if(h==720)
-        fps = fopen(R"(d:\clips\v210_720.yuv)", "rb");
-
-    fseek(fps, 0, SEEK_END);
-    int nLength = ftell(fps);
-    rewind(fps);
-    nLength = std::min<int>(nLength,w*h* 10);
-    fread(sourceBuffer, nLength, 1, fps);
-    fclose(fps);
-
-    memset(targetUYVY8_Buffer, 0, w*h* 10);
-    memset(targetYUV10Packing_Buffer, 0, w*h* 10);
-
-    v210::to_UYVY10packing(sourceBuffer, targetYUV10Packing_Buffer, w,h);
-    {
-        fpD = fopen( getDestFilePath("v210_to_UYVY10"), "wb");
-        fwrite(targetYUV10Packing_Buffer, w*h* 6, 1, fpD);
-        fclose(fpD);
-    }
-
-    v210::to_UYVY8(sourceBuffer, targetUYVY8_Buffer,  w,h);
-    {
-       fpD= fopen(getDestFilePath("v210_to_UYVY8"), "wb");
-        fwrite(targetUYVY8_Buffer, w*h * 6, 1, fpD);
-        fclose(fpD);
-    }
-
-    UYVY8bit::to_UYVY10bitBytePacking(targetUYVY8_Buffer, targetYUV10Packing_Buffer,  w,h);
-    {
-       fpD= fopen( getDestFilePath("UYVY8bit_to_UYVY10bit"), "wb");
-        fwrite(targetYUV10Packing_Buffer, w*h * 6, 1, fpD);
-        fclose(fpD);
-    }
-
-    memset(targetYUV10Packing_Buffer, 0, w*h * 10);
-    UYVY8bit::to_v210(targetUYVY8_Buffer, targetYUV10Packing_Buffer,  w,h);
-    {
-        fpD= fopen(  getDestFilePath("UYVY8bit_to_v210"), "wb");
-        fwrite(targetYUV10Packing_Buffer, w*h * 6, 1, fpD);
-        fclose(fpD);
-    }
-
-}
-void test_10packing(const char *_destFolder)
-{ 
-    char destFolder[255];
-    strcpy(destFolder,_destFolder);
-    strcat(destFolder,"\\10packing\\");
-    std::filesystem::create_directories(destFolder);
-    
-    // makeBlackWhite();
-    int w = 1920;
-    int h = 1080;
-    uint8_t* targetBuffer = new uint8_t[1920 * 1080 * 10];
-    uint8_t* sourceBuffer = new uint8_t[1920 * 1080 * 10];
-    FILE* fps, * fpD;
-
-     fps= fopen(  R"(f:\Codes\backend\Test\clips\UYVY_422p10_LE_BytePacking.yuv)", "rb");
-
-    fseek(fps, 0, SEEK_END);
-    int nLength = ftell(fps);
-    rewind(fps);
-    nLength = std::min<int>(nLength, 1920 * 1080 * 10);
-    fread(sourceBuffer, nLength, 1, fps);
-    fclose(fps);
-
-    memset(targetBuffer, 0, 1920 * 1080 * 6);
-    UYVY10bitBytePacking::to_V210(sourceBuffer, targetBuffer, 1920, 1080);
-     fpD= fopen(  getDestFilePath("10packing_to_V210_1920x1080.yuv"), "wb");
-    fwrite(targetBuffer, 1920 * 1080 * 6, 1, fpD);
-    fclose(fpD);
-
-
-    memset(targetBuffer, 0, 1920 * 1080 * 6);
-    UYVY10bitBytePacking::to_yuv4448bit(sourceBuffer, targetBuffer, 1920, 1080);
-     fpD= fopen(  getDestFilePath("UYVY10bitBytePacking_to_yuv4448bit_1920x1080.yuv"), "wb");
-    fwrite(targetBuffer, 1920 * 1080 * 10, 1, fpD);
-    fclose(fpD);
-}
-
-int main(int argc,char*argv[])
+void test_V210_toAny(const char* _destFolder, int w, int h)
 {
-   // return action_PA16_to_V210(argc, argv);
-   // return action_scaler1080(argc, argv);
+	char destFolder[255];
+	strcpy(destFolder, _destFolder);
+	strcat(destFolder, "\\v210\\");
+	std::filesystem::create_directories(destFolder);
 
-	//checkRange(argv[1]);
-//	return 0;
-    test_v210_toAny_FPS();
-    return 0;
-    const char *destFolder = R"(d:\clips\)";
-   // test_p216(destFolder);
-    //test_pA16(destFolder);
-   // test_10packing(destFolder);
+	uint8_t* targetUYVY8_Buffer = new uint8_t[w * h * 10];
+	uint8_t* targetYUV10Packing_Buffer = new uint8_t[w * h * 10];
+	uint8_t* sourceBuffer = new uint8_t[w * h * 10];
+	FILE* fps, * fpD;
+	if (h == 1080)
+		fps = fopen(R"(d:\clips\v210_1080.yuv)", "rb");
+	else if (h == 720)
+		fps = fopen(R"(d:\clips\v210_720.yuv)", "rb");
 
-    //test_V210_toAny(destFolder,1920,1080);
-   // test_V210_toAny(destFolder, 1280,720);
-    test_1080V210(destFolder);
-    return 0;
+	fseek(fps, 0, SEEK_END);
+	int nLength = ftell(fps);
+	rewind(fps);
+	nLength = std::min<int>(nLength, w * h * 10);
+	fread(sourceBuffer, nLength, 1, fps);
+	fclose(fps);
+
+	memset(targetUYVY8_Buffer, 0, w * h * 10);
+	memset(targetYUV10Packing_Buffer, 0, w * h * 10);
+
+	v210::to_UYVY10packing(sourceBuffer, targetYUV10Packing_Buffer, w, h);
+	{
+		fpD = fopen(getDestFilePath("v210_to_UYVY10"), "wb");
+		fwrite(targetYUV10Packing_Buffer, w * h * 6, 1, fpD);
+		fclose(fpD);
+	}
+
+	v210::to_UYVY8(sourceBuffer, targetUYVY8_Buffer, w, h);
+	{
+		fpD = fopen(getDestFilePath("v210_to_UYVY8"), "wb");
+		fwrite(targetUYVY8_Buffer, w * h * 6, 1, fpD);
+		fclose(fpD);
+	}
+
+	UYVY8bit::to_UYVY10bitBytePacking(targetUYVY8_Buffer, targetYUV10Packing_Buffer, w, h);
+	{
+		fpD = fopen(getDestFilePath("UYVY8bit_to_UYVY10bit"), "wb");
+		fwrite(targetYUV10Packing_Buffer, w * h * 6, 1, fpD);
+		fclose(fpD);
+	}
+
+	memset(targetYUV10Packing_Buffer, 0, w * h * 10);
+	UYVY8bit::to_v210(targetUYVY8_Buffer, targetYUV10Packing_Buffer, w, h);
+	{
+		fpD = fopen(getDestFilePath("UYVY8bit_to_v210"), "wb");
+		fwrite(targetYUV10Packing_Buffer, w * h * 6, 1, fpD);
+		fclose(fpD);
+	}
+
+}
+void test_10packing(const char* _destFolder)
+{
+	char destFolder[255];
+	strcpy(destFolder, _destFolder);
+	strcat(destFolder, "\\10packing\\");
+	std::filesystem::create_directories(destFolder);
+
+	// makeBlackWhite();
+	int w = 1920;
+	int h = 1080;
+	uint8_t* targetBuffer = new uint8_t[1920 * 1080 * 10];
+	uint8_t* sourceBuffer = new uint8_t[1920 * 1080 * 10];
+	FILE* fps, * fpD;
+
+	fps = fopen(R"(f:\Codes\backend\Test\clips\UYVY_422p10_LE_BytePacking.yuv)", "rb");
+
+	fseek(fps, 0, SEEK_END);
+	int nLength = ftell(fps);
+	rewind(fps);
+	nLength = std::min<int>(nLength, 1920 * 1080 * 10);
+	fread(sourceBuffer, nLength, 1, fps);
+	fclose(fps);
+
+	memset(targetBuffer, 0, 1920 * 1080 * 6);
+	UYVY10bitBytePacking::to_V210(sourceBuffer, targetBuffer, 1920, 1080);
+	fpD = fopen(getDestFilePath("10packing_to_V210_1920x1080.yuv"), "wb");
+	fwrite(targetBuffer, 1920 * 1080 * 6, 1, fpD);
+	fclose(fpD);
+
+
+	memset(targetBuffer, 0, 1920 * 1080 * 6);
+	UYVY10bitBytePacking::to_yuv4448bit(sourceBuffer, targetBuffer, 1920, 1080);
+	fpD = fopen(getDestFilePath("UYVY10bitBytePacking_to_yuv4448bit_1920x1080.yuv"), "wb");
+	fwrite(targetBuffer, 1920 * 1080 * 10, 1, fpD);
+	fclose(fpD);
+}
+
+int main(int argc, char* argv[])
+{
+	// return action_PA16_to_V210(argc, argv);
+	// return action_scaler1080(argc, argv);
+
+	 //checkRange(argv[1]);
+ //	return 0;
+	test_v210_toAny_FPS();return 0;
+	const char* destFolder = R"(d:\clips\)";
+	// test_p216(destFolder);
+	 //test_pA16(destFolder);
+	// test_10packing(destFolder);
+
+	 //test_V210_toAny(destFolder,1920,1080);
+	// test_V210_toAny(destFolder, 1280,720);
+	test_1080V210(destFolder);
+	return 0;
 }
--- source/v210_toAny.h
+++ source/v210_toAny.h
@@ -3,6 +3,8 @@
 #include <type_traits>
 #include "putbits.h"
 #include "pixelHeader.h"
+#include <cstring>
+#include <iostream>
 namespace v210
 {
 	void to_UYVY10packing(uint8_t *pV210, uint8_t *pUyvy8, int width, int height)
@@ -139,7 +141,7 @@ namespace v210
 				int sx = (int)(dx * scale_x + 0.5f); // 源图像采样列，四舍五入
 				int group_idx = sx / src_pixels_per_group;
 				int pixel_in_group = sx % src_pixels_per_group;
-
+				
 				Pixel2_1xV210 *p = ((Pixel2_1xV210 *)buffer) + group_idx;
 				int y = 0, u = 0, v = 0;
 				switch (pixel_in_group)
@@ -155,4 +157,251 @@ namespace v210
 			}
 		}
 	}
+			//eighth
+			//2160/270    8/1  24-->3
+			// |----||----||----||----| |----||----||----||----| |----||----||----|
+			// 012345012345012345012345 012345012345012345012345 012345012345012345012345
+			// |-------|-------|------- |-------|-------|------- |-------|---|---|---|---
+			// 0       2       4        0       2       4
+			// 4Group -->3 pixel
+			// 
+			// half
+			// 1080/960  2/1   12-->6
+			// 
+			// quarter
+			//2160/960    4/1
+			//1080/270    4/1   24-->6  12-->3
+			// |----||----| |----||----| |----||----||----||----||----||----||----|
+			// 012345012345 012345012345 012345012345012345012345012345012345012345012345
+			// |---|---|--- |---|---|--- |---|---|---|---|---|---|---|---|---|---|---|---
+			// 0   4   2    0   4   2    0   4   2
+			// 2Group -->3pixel
+
+	void V210_to_YUV_8_to_1(uint8_t* small960Frame, uint8_t* fullFrame,
+		int width, int height,
+		int destWidth, int destHeight)
+	{
+		float scale_x = (float)width / destWidth;
+		float scale_y = (float)height / destHeight;
+
+		int t_width = destWidth * 3; // 每行字节数
+#define GET_linePitch(w) (((w + 47) / 48) * 48)
+		int32_t linePitch = GET_linePitch(width);
+		int32_t lineSize = (width / 48 + ((width % 48) ? 1 : 0)) * 128;
+
+		int32_t  step = linePitch / 24;
+		for (int dy = 0; dy < destHeight; ++dy)
+		{
+			uint8_t* cur = small960Frame + dy * t_width;
+			int sy = (int)(dy * scale_y + 0.5f); // 源图像采样行，四舍五入
+			Pixel2_1xV210* p = (Pixel2_1xV210*)(fullFrame + sy * lineSize);
+			for (int dx = 0; dx < step; ++dx)
+			{
+				handleYUV_3840(cur, p->Y0, p->U0, p->V0); p++;
+				handleYUV_3840(cur, p->Y2, p->U1, p->V1); p++;
+				handleYUV_3840(cur, p->Y4, p->U2, p->V2); p++;
+				p++;
+			}
+		}
+	}
+	void V210_to_YUV_4_to_1(uint8_t* small960Frame, uint8_t* fullFrame,
+		int width, int height,
+		int destWidth, int destHeight)
+	{
+		float scale_x = (float)width / destWidth;
+		float scale_y = (float)height / destHeight;
+
+		int t_width = destWidth * 3; // 每行字节数
+#define GET_linePitch(w) (((w + 47) / 48) * 48)
+		int32_t linePitch = GET_linePitch(width);
+		int32_t lineSize = (width / 48 + ((width % 48) ? 1 : 0)) * 128;
+
+		int32_t  step = linePitch / 12;
+		for (int dy = 0; dy < destHeight; ++dy)
+		{
+			uint8_t* cur = small960Frame + dy * t_width;
+			int sy = (int)(dy * scale_y + 0.5f); // 源图像采样行，四舍五入
+			Pixel2_1xV210* p = (Pixel2_1xV210*)(fullFrame + sy * lineSize);
+			for (int dx = 0; dx < step; ++dx)
+			{
+				handleYUV_3840(cur, p->Y0, p->U0, p->V0);
+				handleYUV_3840(cur, p->Y4, p->U2, p->V2);
+				p++;
+				handleYUV_3840(cur, p->Y2, p->U1, p->V1);
+				p++;
+			}
+		}
+	}
+	void V210_to_YUV_2_to_1(uint8_t* small960Frame, uint8_t* fullFrame,
+		int width, int height,
+		int destWidth, int destHeight)
+	{
+		float scale_x = (float)width / destWidth;
+		float scale_y = (float)height / destHeight;
+
+		int t_width = destWidth * 3; // 每行字节数
+#define GET_linePitch(w) (((w + 47) / 48) * 48)
+		int32_t linePitch = GET_linePitch(width);
+		int32_t lineSize = (width / 48 + ((width % 48) ? 1 : 0)) * 128;
+
+		int32_t  step = linePitch / 12;
+		for (int dy = 0; dy < destHeight; ++dy)
+		{
+			uint8_t* cur = small960Frame + dy * t_width;
+			int sy = (int)(dy * scale_y + 0.5f); // 源图像采样行，四舍五入
+			Pixel2_1xV210* p = (Pixel2_1xV210*)(fullFrame + sy * lineSize);
+			for (int dx = 0; dx < step; ++dx)
+			{
+				handleYUV_3840(cur, p->Y0, p->U0, p->V0);//01
+				handleYUV_3840(cur, p->Y2, p->U1, p->V1);//23
+				handleYUV_3840(cur, p->Y4, p->U2, p->V2);//45
+				p++;
+				handleYUV_3840(cur, p->Y0, p->U0, p->V0);//01
+				handleYUV_3840(cur, p->Y2, p->U1, p->V1);//23
+				handleYUV_3840(cur, p->Y4, p->U2, p->V2);//45
+				p++;
+			}
+		}
+	}
+
+	void Scaler_3840_2160_V210_to_YUV480x270(uint8_t* small480Frame, uint8_t* fullFrame)
+	{
+		uint8_t* line = small480Frame; //dest
+		uint8_t* cur;
+		int32_t  t_width = 0;
+		int32_t  destWidth = 480;
+		t_width = destWidth * 3;
+
+#define GET_linePitch(w) (((w + 47) / 48) * 48)
+
+		int32_t linePitch = GET_linePitch(3840);
+		int32_t lineSize = (3840 / 48 + ((3840 % 48) ? 1 : 0)) * 128;
+
+		int32_t* buffer = nullptr;
+		int32_t  step = linePitch / 24;
+		for (int32_t h = 0; h < 2160; h++)
+		{
+			if (h % 8 != 0)continue;
+			cur = line;
+			buffer = (int32_t*)(fullFrame + h * lineSize);
+			for (int32_t i = 0; i < step; i++)
+			{
+				Pixel2_1xV210* p0 = (Pixel2_1xV210*)buffer;
+				buffer += 4;
+				Pixel2_1xV210* p1 = (Pixel2_1xV210*)buffer;
+				buffer += 4;
+				Pixel2_1xV210* p2 = (Pixel2_1xV210*)buffer;
+				buffer += 4;
+				//Pixel2_1xV210* p3 = (Pixel2_1xV210*)buffer; 
+				//24y-->3y   1/8
+				//01234501 23450123 45012345 
+				//0        2        4 
+				handleYUV_3840(cur, p0->Y0, p0->U0, p0->V0);
+				handleYUV_3840(cur, p1->Y2, p1->U1, p1->V1);
+				handleYUV_3840(cur, p2->Y4, p2->U2, p2->V2);
+
+				buffer += 4;
+			}
+			line += t_width;
+		}
+	}
+
+
+	void Scaler_3840_2160_V210_to_YUV960x540(uint8_t* small960Frame, uint8_t* fullFrame)
+	{
+		uint8_t* line = small960Frame; //dest
+		uint8_t* cur;
+		int32_t  t_width = 0;
+		int32_t  destWidth = 960;
+		t_width = destWidth * 3;
+
+#define GET_linePitch(w) (((w + 47) / 48) * 48)
+
+		int32_t linePitch = GET_linePitch(3840);
+		int32_t lineSize = (3840 / 48 + ((3840 % 48) ? 1 : 0)) * 128;
+
+		int32_t* buffer = nullptr;
+		int32_t  step = linePitch / 12;
+		for (int32_t h = 0; h < 2160; h++)
+		{
+			if (h % 4 != 0)continue;
+			cur = line;
+			buffer = (int32_t*)(fullFrame + h * lineSize);
+			for (int32_t i = 0; i < step; i++)
+			{
+				Pixel2_1xV210* p = (Pixel2_1xV210*)buffer;
+				buffer += 4;
+				Pixel2_1xV210* p1 = (Pixel2_1xV210*)buffer;
+				//y0y1y2y3 y4y5y0y1 y2y3y4y5
+				//12y-->3y   1/4
+				//0123 4501 2345 
+				//0    4    2
+				handleYUV_3840(cur, p->Y0, p->U0, p->V0);
+				handleYUV_3840(cur, p->Y4, p->U2, p->V2);
+				handleYUV_3840(cur, p1->Y2, p1->U1, p1->V1);
+
+				buffer += 4;
+			}
+			line += t_width;
+		}
+	}
+	void Scaler_1920_1080_V210_to_YUV480x270(uint8_t* small480Frame, uint8_t* fullFrame)
+	{
+		uint8_t* line = small480Frame; //dest
+		uint8_t* cur = nullptr;
+		int32_t  destWidth = 480;
+		int32_t  t_width = destWidth * 3;
+
+#define GET_linePitch(w) (((w + 47) / 48) * 48)
+
+		int32_t  linePitch = GET_linePitch(1920);
+		int32_t  lineSize = (1920 / 48 + ((1920 % 48) ? 1 : 0)) * 128;
+		uint16_t U1_10b;
+		uint16_t Y1_10b;
+		uint16_t V1_10b;
+		uint16_t Y2_10b;
+		uint16_t U2_10b;
+		uint16_t Y3_10b;
+		uint16_t V2_10b;
+		uint16_t Y4_10b;
+		uint16_t U3_10b;
+		uint16_t Y5_10b;
+		uint16_t V3_10b;
+		uint16_t Y6_10b;
+		int32_t* buffer = nullptr;
+		int32_t  step = linePitch / 12;
+		for (int32_t h = 0; h < 1080; h++)
+		{
+			if (h % 4 != 0)
+				continue;
+			cur = line;
+			buffer = (int32_t*)(fullFrame + h * lineSize);
+			
+			//each 
+			for (int32_t i = 0; i < step; i++)
+			{
+				U1_10b = (0x3FF & buffer[0]);
+				Y1_10b = (0xFF300 & buffer[0]) >> 10;
+				V1_10b = (0x3ff00000 & buffer[0]) >> 20;
+				//Y2_10b = (0x3FF & buffer[1]);
+				U2_10b = (0xFF300 & buffer[1]) >> 10;
+				Y3_10b = (0x3ff00000 & buffer[1]) >> 20;
+				V2_10b = (0x3FF & buffer[2]);
+				//Y4_10b = (0xFF300 & buffer[2]) >> 10;
+				U3_10b = (0x3ff00000 & buffer[2]) >> 20;
+				Y5_10b = (0x3FF & buffer[3]);
+				V3_10b = (0xFF300 & buffer[3]) >> 10;
+				//Y6_10b = (0x3ff00000 & buffer[3]) >> 20;
+
+				handleYUV_3840(cur, Y1_10b, U1_10b, V1_10b);
+				handleYUV_3840(cur, Y3_10b, U2_10b, V2_10b);
+				handleYUV_3840(cur, Y5_10b, U3_10b, V3_10b);
+
+				buffer += 8;
+			}
+			line += t_width;
+		}
+	}
+
+
 };
